48e9bab283834742dd9ebad242830d07c306b576
joe beuckman <joebeuckman@gmail.com>
2013-11-03 13:04:43 -0600

    updated spec

--- drupal_spec.js
+++ drupal_spec.js
@@ -1,174 +1,172 @@
 describe("Drupal Tests", function() {
 
     var drupal = require('drupal/drupal');
-    drupal.setRestPath('http://localhost:8888/servicestest/', 'api');
+
+    describe("can create account & login", function() {
+        
+        var username = 'drupalspec'+createRandomString(8);
+        var user = {
+            name: username,
+            pass: createRandomString(8),
+            mail: username+'@drupalspec.com',
+            status: 1
+        };
+        var uid = 0;
+            
+
+        it("calls system.connect", function() {
+            
+            var connected = false;
+            
+            runs(function(){
+                
+                drupal.systemConnect(
+                    function(responseData) {
+                        uid = responseData.user.uid;
+                        Ti.API.info("systemConnect got uid "+uid);
+                        connected = true;
+                    },
+                    function(responseData) {
+                        connected = false;
+                    }
+                );
+            });
+                
+            waitsFor(function(){ return connected; }, 'problem connecting', 5000);
+
+        });
+        
+        it("logs out if necessary", function() {
+            
+            var loggedout = false;
+            
+            runs(function() {
+                if (uid != 0) {
+                    Ti.API.info("logging out for user "+uid);
+                    drupal.logout(
+                        function(){
+                            loggedout = true;
+                        }, 
+                        function() {
+                            loggedout = false;
+                        }
+                    );
+                }
+                else {
+                    Ti.API.info("already logged out - proceeding...");
+                    loggedout = true;
+                }
+            });
+            
+            waitsFor(function(){ return loggedout; }, "could not log out", 5000);
+            
+        });
 
 
-	describe("can create account & login", function() {
-		
-		var username = 'drupalspec'+createRandomString(8);
-		var user = {
-			name: username,
-			pass: createRandomString(8),
-			mail: username+'@drupalspec.com',
-			status: 1
-		};
-		var uid = 0;
-			
+        it("registers an account", function() {
 
-		it("calls system.connect", function() {
-			
-			var connected = false;
-			
-			runs(function(){
-				
-				drupal.systemConnect(
-					function(responseData) {
-						uid = responseData.user.uid;
-						Ti.API.info("systemConnect got uid "+uid);
-						connected = true;
-					},
-					function(responseData) {
-						connected = false;
-					}
-				);
-			});
-				
-			waitsFor(function(){ return connected; }, 'problem connecting', 5000);
+            var done = false;
+            var error = '';
+            var response = '';
+                        
+            runs(function(){
+                
+                drupal.createAccount(user, 
+                    //success
+                    function(res) {
+                        response = res;
+                        done = true;
+                    },
+                    //failure
+                    function(e) {
+                        error = e;
+                        done = true;
+                    }
+                );
+            });
+            
+            waitsFor(function(){ return done; }, 'problem creating account', 5000);
+            
+            runs(function(){
+                expect(error).toEqual('');
+            });
 
-		});
-		
-		it("logs out if necessary", function() {
-			
-			var loggedout = false;
-			
-			runs(function() {
-				if (uid != 0) {
-					Ti.API.info("logging out for user "+uid);
-					drupal.logout(
-						function(){
-							loggedout = true;
-						}, 
-						function() {
-							loggedout = false;
-						}
-					);
-				}
-				else {
-					Ti.API.info("already logged out - proceeding...");
-					loggedout = true;
-				}
-			});
-			
-			waitsFor(function(){ return loggedout; }, "could not log out", 5000);
-			
-		});
+        });
 
 
-		it("registers an account", function() {
+        it("can log in", function() {
+            
+            var loggedin = false;
+            
+            // login as the previously created test user
+            runs(function() {
+                drupal.login(user.name, user.pass,
+                    function(data) {
+                        Ti.API.info('spec login succeeded with uid '+data.uid);
+                        uid = data.uid;
+                        loggedin = true;
+                    },
+                    function(err) {
+                        Ti.API.error(err);
+                        loggedin = false;
+                    }
+                );
+            });
 
-			var done = false;
-			var error = '';
-			var response = '';
-						
-			runs(function(){
-				
-				drupal.createAccount(user, 
-					//success
-					function(res) {
-						response = res;
-						done = true;
-					},
-					//failure
-					function(e) {
-						error = e;
-						done = true;
-					}
-				);
-			});
-			
-			waitsFor(function(){ return done; }, 'problem creating account', 5000);
-			
-			runs(function(){
-				expect(error).toEqual('');
-			});
+            waitsFor(function(){ return loggedin; }, 'logged in', 5000);
+            
+        });
 
-		});
+        
+        
+        it("can load user entity", function() {
+            
+            var done = false;
+            
+            runs(function() {
+                drupal.getResource('user/'+uid, null, 
+                    function(data) {
+                        done = true;
+                    },
+                    function(err) {
+                        Ti.API.error(err);
+                        done = false;
+                    }
+                );
+            });
 
+            waitsFor(function(){ return done; }, 'loaded my user', 5000);
+        });
 
-		it("can log in", function() {
-			
-			var loggedin = false;
-			
-			// login as the previously created test user
-			runs(function() {
-				drupal.login(user.name, user.pass,
-					function(data) {
-					    Ti.API.info('spec login succeeded with uid '+data.uid);
-					    uid = data.uid;
-						loggedin = true;
-					},
-					function(err) {
-						Ti.API.error(err);
-						loggedin = false;
-					}
-				);
-			});
+        
+        
+        xit("can create a node", function() {
+            
+            var done = false;
+            
+            var node = {
+                type: "article",
+                title: drupal.basicField("test node title"),
+                body: drupal.basicField("test node body"),              
+            };
+            
+            runs(function() {
+                drupal.postResource('node', node, 
+                    function() {
+                        Ti.API.info('POST succeeded');
+                        done = true;
+                    },
+                    function(err) {
+                        Ti.API.error(err);
+                        done = false;
+                    }
+                );
+            });
 
-			waitsFor(function(){ return loggedin; }, 'logged in', 5000);
-			
-		});
-
-		
-		
-		it("can load user entity", function() {
-			
-			var done = false;
-			
-			runs(function() {
-				drupal.getResource('user/'+uid, null, 
-					function(data) {
-						done = true;
-					},
-					function(err) {
-						Ti.API.error(err);
-						done = false;
-					}
-				);
-			});
-
-			waitsFor(function(){ return done; }, 'loaded my user', 5000);
-		});
-
-		
-		
-		xit("can create a node", function() {
-			
-			var done = false;
-			
-			var node = {
-				type: "article",
-				title: drupal.basicField("test node title"),
-				body: drupal.basicField("test node body"),				
-			};
-			
-			runs(function() {
-				drupal.postResource('node', node, 
-					function() {
-					    Ti.API.info('POST succeeded');
-						done = true;
-					},
-					function(err) {
-						Ti.API.error(err);
-						done = false;
-					}
-				);
-			});
-
-			waitsFor(function(){ return done; }, 'posted a node', 5000);
-		});
-		
-		
+            waitsFor(function(){ return done; }, 'posted a node', 5000);
+        });
+        
+        
 
         it("can log out", function() {
             
@@ -194,21 +192,21 @@ describe("Drupal Tests", function() {
             
         });
 
-	});
-	
+    });
+    
 
-	describe("deals with Drupal data types", function(){
-		
-		it("serializes filter parameters for drupal", function(){
-			var params = {
-				'arg[]': [1,2,3]
-			};
-			var request = drupal.serializeDrupalViewsFilter(params);
-			
-			expect(decodeURIComponent(request)).toEqual('arg[]=1&arg[]=2&arg[]=3');
-		});
+    describe("deals with Drupal data types", function(){
+        
+        it("serializes filter parameters for drupal", function(){
+            var params = {
+                'arg[]': [1,2,3]
+            };
+            var request = drupal.serializeDrupalViewsFilter(params);
+            
+            expect(decodeURIComponent(request)).toEqual('arg[]=1&arg[]=2&arg[]=3');
+        });
 
-	});
+    });
 
 }); 
 
